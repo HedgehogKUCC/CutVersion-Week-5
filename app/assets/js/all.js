ec2d63e1da87c98e6bd4ab89811a65d7e4045b2c
HedgehogKU <ccc908925@gmail.com>
2020-05-23 22:10:27 +0800

    改用 ejs 判斷切換樣式

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
-  $('#menuList').click(function(e) {
-    e.preventDefault();
-    if (e.target.nodeName !== 'A') {
-      return;
-    }
-    let li = e.target.parentNode;
-    $(li).toggleClass('active');
-    $(li).siblings().removeClass('active');
-  })
+  // $('#menuList').click(function(e) {
+  //   e.preventDefault();
+  //   if (e.target.nodeName !== 'A') {
+  //     return;
+  //   }
+  //   let li = e.target.parentNode;
+  //   $(li).toggleClass('active');
+  //   $(li).siblings().removeClass('active');
+  // })
 });
