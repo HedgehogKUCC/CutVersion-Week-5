241db20aece2e4a4a5e50c9efd6f282bcebc872a
HedgehogKU <ccc908925@gmail.com>
2020-05-23 20:55:52 +0800

    切換點選樣式

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
-  console.log('Hello Bootstrap4');
+  $('#menuList').click(function(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'A') {
+      return;
+    }
+    let li = e.target.parentNode;
+    $(li).toggleClass('active');
+    $(li).siblings().removeClass('active');
+  })
 });
