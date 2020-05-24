ea38b203e40256ee775b71e7d4eb632007514bd3
HedgehogKU <ccc908925@gmail.com>
2020-05-24 20:14:19 +0800

    admin eyeModal

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  $('#onReply').click(function () {
+  $('#onReply').click(function() {
     $('.bodyText').css('width', '100%');
     $('.bodyText').css('height', '290px');
   })
@@ -7,5 +7,13 @@ $(document).ready(() => {
   $('#onCancel').click(function() {
     $('.bodyText').css('width', '0%');
     $('.bodyText').css('height', '0px');
+  })
+
+  $('#iconMoreModal').click(function() {
+    if ($(this).text().trim() === 'expand_more') {
+      $(this).text('keyboard_arrow_up')
+    } else {
+      $(this).text('expand_more')
+    }
   })
 });
